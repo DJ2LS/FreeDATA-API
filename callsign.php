4a1edf4d65f9f650a59c66776ad89868390a8de4
DJ2LS <75909252+DJ2LS@users.noreply.github.com>
2023-07-05 09:13:16 +0200

    first working reverse lookup table

--- callsign.php
+++ callsign.php
@@ -34,23 +34,63 @@ if($conn->connect_error){
 	$result = $conn->query("SELECT callsign, dxcallsign FROM summary");
 	if ($result->num_rows > 0) {
 	  // output data of each row
-	  while($row = $result->fetch_assoc()) {
-		  $rows[] = $row;
-			
 
-
+		while($row = $result->fetch_assoc()) {
+			//print($row["callsign"]);
+			$rows[] = $row["callsign"];
+			$rows[] = $row["dxcallsign"];
 	  }
+			
+		// Remove duplicate values
+		$uniqueCallsignArray = array_unique($rows);
+
+
+		foreach ($uniqueCallsignArray as $key => $value) {
+
+			$crc24Value = crc24_openpgp($value);
+			$crc24Value = sprintf("%08x", $crc24Value);
+			$crc24HexTrimmed = ltrim($crc24Value, '0');
+
+			$jsonArray[$value] = $crc24HexTrimmed; // Assign each value to the corresponding key in the JSON array
+		}
+
+		$jsonString = json_encode($jsonArray);
+		print($jsonString);
 		
-		$json_string = json_encode($rows);
-		exit("{$_GET['callback']}($json_string)");
+		//$json_string = json_encode($rows);
+
+		//exit("{$_GET['callback']}($json_string)");
 		//print trim($json_string,'\\');	
+
+
 	} else {
 	  echo "0 results";
 	}
 		
 }
 	
+// chatgpt generated crc24 checksum
+function crc24_openpgp($data) {
+    $crc = 0xB704CE;
+    $len = strlen($data);
 
+    for ($i = 0; $i < $len; $i++) {
+        $crc ^= (ord($data[$i]) << 16);
+
+        for ($j = 0; $j < 8; $j++) {
+            $crc <<= 1;
+
+            if ($crc & 0x1000000) {
+                $crc ^= 0x864CFB;
+            }
+        }
+    }
+
+
+
+
+    return ($crc & 0xFFFFFF);
+}
 
 
 ?>
